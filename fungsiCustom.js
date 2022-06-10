5da1a96929cae94018725684de397e3b8b252e5f
grarizki <28901232+grarizki@users.noreply.github.com>
2022-06-10 23:49:10 +0700

    add tugas

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,40 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf8", (err, data1) => {
+    if (err) {
+      fnCallback("Error saat read file data1 : " + err, null);
+      return;
+    }
+    fs.readFile(file2, "utf8", (err, data2) => {
+      if (err) {
+        fnCallback("Error saat read file data2 : " + err, null);
+        return;
+      }
+      fs.readFile(file3, "utf8", (err, data3) => {
+        if (err) {
+          fnCallback("Error saat read file data3 : " + err, null);
+          return;
+        }
+        const arrData = [];
+        const jsonData1 = JSON.parse(data1);
+        const jsonData2 = JSON.parse(data2);
+        const jsonData3 = JSON.parse(data3);
+        arrData.push(getKataKedua(jsonData1.message));
+        arrData.push(getKataKedua(jsonData2[0].message));
+        arrData.push(getKataKedua(jsonData3[0].data.message));
+
+        fnCallback(null, arrData);
+      });
+    });
+  });
+};
+
+const getKataKedua = (kalimat) => {
+  const arrKalimat = kalimat.split(" ");
+  return arrKalimat[1];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
